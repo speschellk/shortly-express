626451db377de3b5d065c7643b70182068b57874
speschellk <kaychristensen@gmail.com>
2017-01-17 21:09:37 -0800

    Remove conlogs

--- app/models/user.js
+++ app/models/user.js
@@ -3,31 +3,11 @@ var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
 
-// need to initialize this model class
 var User = db.Model.extend({
   tableName: 'users',
   hasTimestamps: false,
 
-  initialize: function(attr) {
-    // this.on('creating', function(model, attr, options) {
-    //   bcrypt.genSalt(16, function(err, salt) {
-    //     if (err) {
-    //       console.log('Error generating salt', err);
-    //     } else {
-    //       bcrypt.hash(attr.password, salt, null, function(err, hashedPW) {
-    //         if (err) {
-    //           console.log('Error hashing password', err);
-    //         } else {
-    //           console.log(attr, attr.username, salt, hashedPW);
-    //           db.knex('users').where({ username: attr.username }).update({
-    //             salt: salt, password: hashedPW
-    //           }).asCallback(function() {});
-    //         }
-    //       });
-    //     }
-    //   });
-    // });
-  }
+  initialize: function() {}
 });
 
 module.exports = User;
